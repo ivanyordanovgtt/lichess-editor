0206739337f55cb62270fe99cd742f729209bcce
invanyordanvogt <166876998+ivanyordanovgtt@users.noreply.github.com>
2024-04-13 00:39:30 +0200

    Delete lichess-editor-master directory

--- lichess-editor-master/python/get_local_storage_js.py
+++
@@ -1,8 +0,0 @@
-JS = """
-var ls = {}, keys = Object.keys(localStorage);
-for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    ls[key] = localStorage.getItem(key);
-}
-return ls;
-"""
